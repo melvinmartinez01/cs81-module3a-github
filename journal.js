38717a12bf922818f9dd432f3bae86c90c29bfb7
Melvin Martinez <martinez_lorenzo_m01@student.smc.edu>
2026-01-15 23:46:32 -0800

    Add grade calculation function

--- journal.js
+++ journal.js
@@ -32,3 +32,20 @@ function groceryRemindList() {
 
   return "Grocery list: " + groceries.join(", ");
 }
+
+//Percentage score to letter grade function
+function calculateGrade(score) {
+  if (score >= 90 && score <= 100) {
+    return "A";
+  } else if (score >= 80 && score < 90) {
+    return "B";
+  } else if (score >= 70 && score < 80) {
+    return "C";
+  } else if (score >= 60 && score < 70) {
+    return "D";
+  } else if (score >= 0 && score < 60) {
+    return "F";
+  } else {
+    return "Invalid score. Please enter a number between 0 and 100.";
+  }
+}
