66ca51257ecef30420469f05d9a18b433a28c123
Melvin Martinez <martinez_lorenzo_m01@student.smc.edu>
2026-01-15 23:01:04 -0800

    Add if and then function to howWasYourDay()

--- journal.js
+++ journal.js
@@ -2,3 +2,16 @@
 function greet(name) {
   return "Hello, " + name + "!";
 }
+
+//Ask user how their day is by using if and then function
+function howWasYourDay(response) {
+  if (response === "good") {
+    return "I'm glad you had a good day!";
+  } else if (response === "okay") {
+    return "Well, it could be worse. At least you're alive!";
+  } else if (response === "bad") {
+    return "Dang, I'm sorry man. Do me a favor? Go hug your daughter.";
+  } else {
+    return "You good bro? Okay? Or is it a bad day?";
+  }
+}
